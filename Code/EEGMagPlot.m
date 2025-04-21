295d0e394180d37387308fe085b77718212d56fd
inpinseptipin <saty@satyartharora.com>
2025-04-20 21:05:21 -0400

    Plot Function Fixed

--- Code/EEGMagPlot.m
+++ Code/EEGMagPlot.m
@@ -1,11 +1,18 @@
 function [] = EEGMagPlot(x,fs)
 
-x = downsample(x,32);
-fftx = fft(x);
+x = downsample(x,128);
+fs = fs./128;
+fftx = fft(x)./fs;
 fftx = fftx';
 fftx = db(abs(fftx(1:end/2,1)));
+xAxis = zeros(floor(size(x,2)/2),1);
+for i = 1:size(xAxis,1)
+    xAxis(i,1) = i .* fs/2/size(xAxis,1);
+end
 figure;
-plot(fftx);
+plot(xAxis,fftx);
+xlabel("Hz")
+ylabel("dBv")
 hold on;
 end
 
--- Code/EEGPlot.m
+++ Code/EEGPlot.m
@@ -1,8 +1,26 @@
-function [] = EEGPlot(x,fs)
+function [] = EEGPlot(x,fs,rowIndex,data,peakNames)
+
 xAxis = 0:1/fs:length(x)/fs;
 xAxis = xAxis(:,1:end-1)';
 figure;
 plot(xAxis,x);
+hold on;
+for i = 1:size(peakNames,1)
+    pattern = strcat(peakNames(i,:),'_');
+    matchingColumns = startsWith(data.Properties.VariableNames,pattern);
+    peakData = data(rowIndex,find(matchingColumns == 1));
+    peakMatrix = table2array(peakData);
+    if ~isnan(peakMatrix)
+        position = peakMatrix(:,3);
+        positionInSeconds = position/1000;
+        positionInSampleIndex = round(positionInSeconds*fs);
+        amp = x(1,positionInSampleIndex);
+        plot(positionInSeconds, amp, 'v', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r');
+        text(positionInSeconds, amp + 0.2, peakNames(i,:), 'Color', 'k');
+        hold on;
+    end
+    
+end
 xlabel("Time (s)")
 ylabel("Amplitude (uV)")
 hold on;
