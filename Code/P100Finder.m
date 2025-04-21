586f1fd7194ba72108c8b4ddfe7747de124438a9
inpinseptipin <saty@satyartharora.com>
2025-04-21 15:26:08 -0400

    P100 Finder Algo v.01

--- Code/P100Finder.m
+++ Code/P100Finder.m
@@ -5,39 +5,26 @@ function [p100Location] = P100Finder(x,fs)
 startInterval = (80/1000) * fs;
 endInterval = (130/1000)*fs;
 
-x1 = x(startInterval:endInterval,1);
-xAxis = 0:1/fs:length(x1)/fs;
-xAxis = xAxis(:,1:end-1)';
-figure;
-plot(xAxis,x1);
-hold on;
+[zAmp,zLoc] = findpeaks(x,'NPeaks',100);
 
-z = findpeaks(x1,'NPeaks',3);
-p100Location = [];
-for i = 1:size(z,1)
-    p100Location = [p100Location;find(x == z(i,1))];
-end
-p100Location = p100Location./8000;
+p100Location = [zAmp,zLoc];
 
-
-fftx = fft(x1,fs)./fs;
-fftx = db(abs(fftx(1:end/2,1)));
-maxFreq = max(fftx);
-xAxis = zeros(floor(size(fftx,1)),1);
-for i = 1:size(xAxis,1)
-    xAxis(i,1) = i .* fs/2/size(xAxis,1);
+subP100Location = [];
+for i = 1:size(p100Location,1)
+    if p100Location(i,2) >= startInterval && p100Location(i,2) <= endInterval
+        subP100Location = [subP100Location ; [p100Location(i,:)];                                                                                                                                                        ]
+    end
 end
 
+p100Location = subP100Location;
 
+subP100Location = p100Location(1,:);
+for i = 1 : size(p100Location,1)
+    if subP100Location(1,1) < p100Location(i,1)
+        subP100Location = p100Location(i,:);
+    end    
+end
 
-figure;
-plot(xAxis,fftx);
-hold on;
-maxFreqLocation = find(fftx == maxFreq);
-maxFreqValue = xAxis(maxFreqLocation,1);
-
-
-
-
+p100Location = subP100Location;
 end
 
